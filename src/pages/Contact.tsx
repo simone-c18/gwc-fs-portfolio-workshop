20c946cf1de3766487950836e23ca60890f1954f
imanimcq <imanimcquay@gmail.com>
2026-03-08 12:49:42 -0400

    added contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,46 @@
+import {Phone, Code, Link, Mail} from 'lucide-react';
 function Contact() {
-  return <div>Contact Page</div>;
+  return(  
+   <main className="w-screen flex flex-col items-start">
+      <div className="w-full h-screen bg-[#FFD3E7]">
+        <p className="mt-[4vh] ml-[4vw] font-bold text-lg text-left">
+          imani-mcquay.com
+        </p>
+        <div className="mt-[11vh] text-left ml-[5vw] leading-18">
+          <h1 className='text-6xl font-bold mb-10'>Contact Me</h1>
+          <p className="flex items-center text-3xl gap-2 py-5">
+            <Phone/>
+            (123)-456-7890
+          </p>
+           <p className="flex items-center text-3xl gap-2 py-5">
+            <Mail/>
+            youremailaddress@gmail.com
+          </p>
+          <p className="flex items-center text-3xl gap-2 py-5">
+            <Code/>
+            <a
+                href = "https://github.com/imanimcq"
+                target = "_blank"
+                rel="noopener noreferrer"
+                className = "underline"
+                >
+                github.com/imanimcq
+            </a>
+          </p>
+          <p className="flex items-center text-3xl gap-2 py-5">
+            <Link/>
+            <a
+                href = "https://www.linkedin.com/in/imani-mcquay/"
+                target = "_blank"
+                rel="noopener noreferrer"
+                className = "underline"
+                >
+                linkedin.com/in/imani-mcquay
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+ );
 }
 export default Contact;
